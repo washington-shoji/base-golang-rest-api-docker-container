61df9ad387914062c6a8835170d613869b0ade8b
washington-shoji <shoji.washington@gmail.com>
2024-05-14 22:28:38 +1000

    -updated env config

--- config/env-config.go
+++ config/env-config.go
@@ -2,15 +2,22 @@ package config
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/joho/godotenv"
 )
 
 func EnvConfig(key string) string {
-	err := godotenv.Load(".env")
+	path, err := os.Getwd()
 	if err != nil {
-		fmt.Print("Error loading .env file")
+		fmt.Printf("Error in path %s", err)
+		log.Fatal(err)
+	}
+	if err := godotenv.Load(filepath.Join(path, ".env")); err != nil {
+		fmt.Printf("Error in connectiong %s", err)
+		log.Fatal(err)
 	}
 	return os.Getenv(key)
 
