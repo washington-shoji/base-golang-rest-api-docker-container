5c2ff855112ffeff064147fb45d169fc84f27de5
washington-shoji <shoji.washington@gmail.com>
2024-06-10 14:30:35 +1000

    -updated integration test connection string

--- integration/todo-service-integration_test.go
+++ integration/todo-service-integration_test.go
@@ -21,7 +21,7 @@ func setup() {
 	var err error
 	connStr := fmt.Sprintf("host=%s user=%s "+
 		"password=%s dbname=%s sslmode=disable",
-		os.Getenv("DATABASE_HOST"), os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB"),
+		"localhost", "postgres", "postgres", "postgres",
 	)
 
 	db, err = sql.Open("postgres", connStr)
